d55cc8494fc072c424e4657a288ddff71df6e3eb
Your Name <you@example.com>
2025-03-24 09:40:45 -0600

    Calculadora Hook UseState

--- input_react/src/App.jsx
+++ input_react/src/App.jsx
@@ -1,15 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import Input from './components/input'
+import Calculadora from './components/calculadora'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <Input titulo={"Calculadora"}/>
+    <Calculadora/>
     </>
   )
 }
